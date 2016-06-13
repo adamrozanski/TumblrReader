e6fcd47ecbf351de4f3d4cac96685eb4394cb0da
adamrozanski <adamrozanski@me.com>
2016-06-14 01:25:06 +0200

    Initial Commit

---
+++ TumblrReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TumblrReader
+//
+//  Created by Adam on 14.06.2016.
+//  Copyright © 2016 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TumblrReader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TumblrReader
+//
+//  Created by Adam on 14.06.2016.
+//  Copyright © 2016 Adam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

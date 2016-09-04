a939436f53b106f6d75f955bcf619e28cd05fa48
adam_rozanski <adamrozanski@me.com>
2016-09-04 23:57:58 +0200

    .gitignore

---
+++ TumblrReader/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TumblrReader
+//
+//  Created by Adam on 14.06.2016.
+//  Copyright © 2016 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
